a5ddeb459d3d3a4c26cb5bac47c84da3bd3c2b55
Firologame <erwan1207@gmail.com>
2020-10-15 10:03:07 +0200

    ajout de fichiers

---
+++ js/main.js
@@ -0,0 +1,55 @@
+let video = document.getElementById('video');
+let canvas1 = document.querySelector('#canvas');
+let text = document.querySelector('#text');
+let ctx1 = canvas1.getContext('2d');
+let animation;
+let qualite = 10; // plus on baisse cette valeur, plus on augmente la qualité (attention à la ram)
+let characters = (" .,:;i1tfLCG08@").split("");
+let msg = '';
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        start();
+    });
+}
+
+function start() {
+    video.play();
+    video.style.display = 'none';
+    canvas1.style.display = 'none';
+
+    anim();
+}
+
+function anim() {
+
+    animation = requestAnimationFrame(anim);
+
+    ctx1.drawImage(video, 0, 0, 640, 480);
+    
+    var imageData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
+    var data = imageData.data;
+
+    msg = '';
+
+    for (let x = 0; x <= canvas1.width; x+=qualite) {
+        for (let y = 0; y <= canvas1.height; y+=qualite) {
+            const element = Array[y];
+            let red = imageData.data[((x * (imageData.width * 4)) + (y * 4)) + 0]; // + 0 = rouge
+            let green = imageData.data[((x * (imageData.width * 4)) + (y * 4)) + 1]; // + 1 = vert
+            let blue = imageData.data[((x * (imageData.width * 4)) + (y * 4)) + 2];  // + 2 = bleu
+
+            // let alpha = imageData.data[((x * (imageData.width * 4)) + (y * 4)) + 3]; // + 3 = alpha
+            
+            let avg = (red + green + blue) / 3; // je fais la moyenne des 3 couleurs pour avoir du N/B
+            
+            msg += characters[Math.round((avg/1.1)/255*characters.length)];
+        }    
+        msg += '<br />';
+    }
+    text.innerHTML = msg;
+}
